8ac8a768884b7b0d3e854f0d3bce0785a03524f7
Ivan Raychev <ivanraychev@Ivans-Mac-mini.local>
2015-04-25 15:15:06 +0300

    added core data basics

--- Tracker/AppDelegate.h
+++ Tracker/AppDelegate.h
@@ -9,10 +9,18 @@
 #import <UIKit/UIKit.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <CoreData/CoreData.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, FBSDKLoginButtonDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 
 @end
